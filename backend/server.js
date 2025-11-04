0b833f706af945769e83574c5a057efbc022f1b9
Anastasiya <anastasia.makienko@gmail.com>
2025-11-04 12:06:20 +0500

    newcommit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = 3001;
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
